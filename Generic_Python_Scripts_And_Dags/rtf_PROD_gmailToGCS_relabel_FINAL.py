2a786d3cfd4c68badcc336b99dc851a27a03a370
drew-lustig <drew-lustig@users.noreply.github.com>
2019-09-17 18:51:38 -0400

    started making changes for NBCU

--- Generic_Python_Scripts_And_Dags/rtf_PROD_gmailToGCS_relabel_FINAL.py
+++ Generic_Python_Scripts_And_Dags/rtf_PROD_gmailToGCS_relabel_FINAL.py
@@ -1,38 +1,40 @@
-from datetime import datetime
-from airflow import DAG
-import json
-from google.oauth2.credentials import Credentials
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow import models
 import base64
+from email.mime.text import MIMEText
+from io import BytesIO
+import json
 import logging
 import os
-from essence.analytics.platform import securedcredentials as secure_creds
-from googleapiclient.discovery import build
-from google.cloud import storage
+from datetime import datetime
+
+from airflow import DAG, models
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from apiclient import errors
-
-
+from essence.analytics.platform import securedcredentials as secure_creds
+from google.cloud import storage
+from google.oauth2.credentials import Credentials
+from googleapiclient import discovery
+import pandas as pd
 
 SCOPES = ['https://mail.google.com/'] # grants all permissions. proceed with caution.
 
 API_NAME = 'gmail'
 API_VERSION = 'v1'
-userId='rtf_data@essenceglobal.com'
+userId='nbcu_analytics_data@essenceglobal.com'
 
 
 def query_for_message_ids(service, search_query):
-    result = service.users().messages().list(userId='me', q=search_query).execute()
+
+    result = service.users().messages().list(userId='me', q=search_query).execute() # Search for messages matching query
     results = result.get('messages')
     if results:
-        msg_ids = [r['id'] for r in results]
+        msg_ids = [r['id'] for r in results] # Put matching messages' IDs into a list
     else:
         msg_ids = []
 
     return msg_ids
 
-def ModifyMessage(gmail_service, userId, msg_id, msg_labels):
+def modify_message(gmail_service, userId, msg_id, msg_labels):
 
     message = gmail_service.users().messages().modify(userId=userId, id=msg_id, body=msg_labels).execute()
 
@@ -42,7 +44,7 @@ def ModifyMessage(gmail_service, userId, msg_id, msg_labels):
     print('Message ID: %s - With Label IDs %s' % (msg_id, label_ids))
 
 
-def CreateMsgLabels():
+def create_msg_labels():
   """Create object to update labels.
 
   Returns:
@@ -50,41 +52,74 @@ def CreateMsgLabels():
   """
   return {'removeLabelIds': [], 'addLabelIds': ['UNREAD', 'INBOX', 'Label_2']}
 
+def get_attachments(service, user_id, msg_id, message=None):
 
-def GetAttachments(service, user_id, msg_id, prefix=""):
-    message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+    if not message:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+    atts = {}
     for part in message['payload']['parts']:
-        if part['filename']:
+        if part['filename'] and part['mimeType'] == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
             if 'data' in part['body']:
-                data=part['body']['data']
+                data = part['body']['data']
             else:
-                att_id=part['body']['attachmentId']
-                att=service.users().messages().attachments().get(userId=user_id, messageId=msg_id,id=att_id).execute()
-                data=att['data']
-            file_data = base64.urlsafe_b64decode(data.encode('utf-8'))
-            path = prefix+part['filename']
+                att_id = part['body']
+                att = service.users().messages().attachments().get(userId=user_id, messageId=msg_id, id=att_id).execute()
+                data = att['data']
+            atts[part['filename']] = BytesIO(base64.urlsafe_b64decode(data.encode('utf-8')))
+    
+    return atts
 
-            with open(path, 'wb') as f:
-                    f.write(file_data)
-            storage_client = storage.Client()
-            bucket = storage_client.get_bucket('rtf_staging')
-            blob = bucket.blob(path)
-            blob.upload_from_filename(path)
+def process_file(file):
+    # TODO
+    tabs = pd.read_excel(file, sheet_name=['Video','Display'], skiprows=2).iloc[:, 1:]
+    errors = None
+    return errors
+
+def send_reply(service, thread_id, headers, filename, errors):
+    
+    if errors:
+        message_text = "something" # TODO
+    else:
+        message_text = "something else" # TODO
+
+    message = MIMEText(message_text)
+    message['to'] = headers['From']
+    message['from'] = 'nbcu_analytics_data@essenceglobal.com'
+    message['subject'] = headers['Subject']
+    body = {'raw': base64.urlsafe_b64encode(message.as_string())}
+    service.users().messages().send(userId='me', body=body).execute()
+
 
 def processEmailScanning():
-  data_key='rtf_data@essenceglobal.com'
-  data_value=secure_creds.getDataFromEssenceVault(data_key)
-  credentials_dict = json.loads(data_value)
-  credentials = Credentials(**credentials_dict)
-  gmail_service=build(API_NAME, API_VERSION, credentials=credentials,cache_discovery=False)
-  messageID = query_for_message_ids(gmail_service,'has:attachment')
-  for i in range(len(messageID)):
-      GetAttachments(gmail_service, 'me', messageID[i], prefix="rtf_")
-      message = gmail_service.users().messages().modify(userId=userId, id=messageID[i], body={'removeLabelIds': [], 'addLabelIds': ['Label_7576572066746469364']}).execute()
 
-dag = DAG('rtf_PROD_gmailToGCS_relabel_FINAL', description='DAG to fetch gmail attachments and label as Processed',
-          schedule_interval=None,
-          start_date=datetime(2019, 8, 24), catchup=False)
+    data_value = secure_creds.getDataFromEssenceVault(userId) # Gets creds from vault
+    credentials_dict = json.loads(data_value) # Loads creds as dict
+    credentials = Credentials(**credentials_dict) # Creates Google credentials from creds dict
+    gmail_service = discovery.build(API_NAME, API_VERSION, credentials=credentials, cache_discovery=False) # Build Gmail API service
+    message_ids = query_for_message_ids(gmail_service, 'has:attachment')
+
+    for msg_id in message_ids:
+        message = gmail_service.users().messages().get(userId='me', id=msg_id).execute()
+        thread_id = message['threadId']
+        payload = message['payload']
+        headers = {header['name']: header['value'] for header in payload['headers']}
+        attachments = get_attachments(gmail_service, 'me', msg_id, message=message)
+        for filename, file in attachments.items():
+            errors = process_file(file)
+            send_reply(gmail_service, thread_id, headers, filename, errors)      
+
+    # for i in range(len(messageID)):
+    #     get_attachments(gmail_service, 'me', messageID[i])
+    #     message = gmail_service.users().messages().modify(userId=userId, id=messageID[i], body={'removeLabelIds': [], 'addLabelIds': ['Label_7576572066746469364']}).execute()
+
+dag = DAG(
+    'rtf_PROD_gmailToGCS_relabel_FINAL',
+    description='DAG to fetch gmail attachments and label as Processed',
+    schedule_interval=None,
+    start_date=datetime(2019, 8, 24),
+    catchup=False
+)
 
 dummy_operator = DummyOperator(task_id='dummy_task', retries=1, dag=dag)
 
